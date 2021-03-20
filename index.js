477824569c1b7fc6a72889643969fe0a04a79d21
IceJonas <jonas.helgi@hotmail.com>
2021-03-20 19:41:10 +0000

    20.03 different emails

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var users = [];
-    var count1 = 1;
+    var countAdd = 1;
 
     $("#btnAdd").click(function() {
         var tempArr = [$("#inpNum1").val(), $("#inpNum2").val(), $("#inpNum3").val(), $("#inpNum4").val(), $("#inpNum5").val(), $("#inpNum6").val(), $("#inpNum7").val(), $("#inpNum8").val()];
@@ -17,25 +17,32 @@ $(document).ready(function() {
         users.push(user);
 
         // prentar í label
-        newLabel(user.nafn, user.stig, count1);
-        count1++;
+        newLabel(user.nafn, user.stig, countAdd);
+        countAdd++;
 
         // Hreinsar allt
         $("#formIn").trigger("reset");
     });
 
-    // Klára leikinn og finna ut í hvaða sæti hver lenti í.
+    // Taka seinasta út
+    $("#btnRemove").click(function() {
+        tempNum = countAdd - 1;
+        $("#labelScore" + tempNum).empty();
+    });
+
+    // Sortera allt
     $("#btnDone").click(function() {
         users.sort((a, b) => b.stig - a.stig);
         len = Object.keys(users).length;
         var j = 0;
-        var count2 = 1;
+        var countSort = 1;
         while (j != len) {
-            $("#labelScore" + count2).empty().append(users[j].nafn + " " + "<span class='badge badge-secondary'>" + users[j].stig + "</span>");
+            $("#labelScore" + countSort).empty().append(users[j].nafn + " " + "<span class='badge badge-secondary'>" + users[j].stig + "</span>");
             j++;
-            count2++;
+            countSort++;
         };
     });
+
 
     // Bætir nafni og stigum við label
     function newLabel(nafn, stig, count) {
